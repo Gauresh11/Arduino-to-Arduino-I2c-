674f7695c94c3c0ec6b703364865d373f750a488
Gauresh11 <gaureshwadekar82@gmail.com>
2022-08-13 04:01:29 +0530

    first commit

---
+++ master/master.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+
+void setup()
+{
+  Wire.begin(); // join i2c bus (address optional for master)
+  Serial.begin(9600);
+}
+
+byte x = 0;
+
+void loop()
+{
+  if(Serial.available()>0)
+  {
+    Wire.beginTransmission(4);
+    byte string = Serial.read();
+    Serial.print((char)string);  
+    Wire.write((char)string);
+    Wire.endTransmission();    // stop transmitting
+}
+  delay(500);
+}
